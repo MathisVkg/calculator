55d0cbc96960d713aa52d160b5c4f1c8ec94e259
MathisVkg <mvk181@gmail.com>
2021-07-13 16:54:43 +0200

    edit

--- assets/script.js
+++ assets/script.js
@@ -32,10 +32,62 @@ buttons.forEach((btn) => {
     btn.addEventListener("click", numberButton)
     // console.log(buttons);
 });
+var stockNumberFirst = "";
+var stockNumberSecond = "";
+var signe = 0;
 function numberButton() {
     let numberClick = event.target.innerText.toLowerCase();
-    console.log(numberClick);
+    if(numberClick < 10) {
+        document.getElementById("writeAnswer").innerHTML += numberClick;
+        stockNumberFirst += numberClick;
+    }
+    if(numberClick === ".") {
+        document.getElementById("writeAnswer").innerHTML += ".";
+        stockNumberFirst += numberClick;
+    }
+    if(numberClick === "x") {
+        stockNumberSecond = stockNumberFirst;
+        stockNumberFirst = "";
+        document.getElementById("writeAnswer").innerHTML += "x";
+        signe += 1;
+    }
+    if(numberClick === "-") {
+        stockNumberSecond = stockNumberFirst;
+        stockNumberFirst = "";
+        document.getElementById("writeAnswer").innerHTML += "-";
+        signe += 2;
+    }
+    if(numberClick === "+") {
+        stockNumberSecond = stockNumberFirst;
+        stockNumberFirst = "";
+        document.getElementById("writeAnswer").innerHTML += "+";
+        signe += 3;
+    }
+    if(numberClick === "÷") {
+        stockNumberSecond = stockNumberFirst;
+        stockNumberFirst = "";
+        document.getElementById("writeAnswer").innerHTML += "÷";
+        signe += 4;
+    }
+    console.log(stockNumberFirst);
+    console.log(stockNumberSecond);
 }
-// document.getElementById("button1").addEventListener("click", () => {
-//     console.log(document.getElementById("button1"));
-// })
+document.getElementById("egalButtonRun").addEventListener("click", () => {
+    if(signe === 1) {
+        let answer = stockNumberFirst *= stockNumberSecond;
+        document.getElementById("writeAnswer").innerHTML = answer;
+    }
+    if(signe === 2) {
+        let answer = stockNumberSecond -= stockNumberFirst;
+        document.getElementById("writeAnswer").innerHTML = answer;
+    }
+    if(signe === 3) {
+        let answer = stockNumberSecond -= stockNumberFirst;
+        document.getElementById("writeAnswer").innerHTML = answer;
+    }
+    if(signe === 4) {
+        let answer = stockNumberSecond /= stockNumberFirst;
+        document.getElementById("writeAnswer").innerHTML = answer;
+    }
+    console.log(answer)
+})
